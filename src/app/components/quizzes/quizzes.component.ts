72e2e8826ea9b212c5dd2f462300b440f6f42d65
Luis Miguel Baez Aponte <lmbaeza@unal.edu.co>
2020-05-03 16:46:50 -0500

    Se agregó la funcionalidad para registrar un quiz en la base de datos

--- src/app/components/quizzes/quizzes.component.ts
+++ src/app/components/quizzes/quizzes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizzesService } from '../../services/quizzes.service'
+import { toInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-quizzes',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizzesComponent implements OnInit {
 
-  constructor() { }
+  statement: string = '';
+  err: string = '';
 
-  ngOnInit() {
+  ans1: string = '';
+  ans2: string = '';
+  ans3: string = '';
+  ans4: string = '';
+
+  radio: number = 1;
+
+  constructor(private quizzesService: QuizzesService) { }
+
+  ngOnInit() {}
+
+  onClickMe() {
+    console.log("statement " + this.statement)
+    console.log("ans1 " + this.ans1)
+    console.log("ans2 " + this.ans2)
+    console.log("ans3 " + this.ans3)
+    console.log("ans4 " + this.ans4)
+    console.log("radio " + this.radio)
+
+    if(this.statement==='' || this.ans1==='' || this.ans2==='' || this.ans3==='' || this.ans4=='') {
+      this.err = "Todos los campos son obligatorios";
+    } else {
+      this.err = "";
+
+      var answers = [this.ans1, this.ans2, this.ans3, this.ans4];
+
+      var correct = this.radio;
+
+      this.quizzesService.registrarCurso(this.statement, answers, correct);
+    }
+  }
+
+  onClick(element) {
+    this.radio = parseInt(element);
   }
 
 }
--- src/app/services/quizzes.service.ts
+++ src/app/services/quizzes.service.ts
@@ -3,45 +3,105 @@ import {Apollo, QueryRef} from 'apollo-angular';
 import gql from 'graphql-tag';
 import { CursoModel } from '../models/curso.model';
 
+type Answer = {
+  id: string;
+  context: string;
+};
+
+type Question = {
+  statement: string;
+}
+
+type Response = {
+  InsertAnswer: Answer;
+};
+
+type ResponseQuestion = {
+  InsertQuestion: Question
+}
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class QuizzesService {
-  
-
   cursos: CursoModel[];
 
   private query: QueryRef<any>;
 
-  constructor(private apollo: Apollo) {
-    // console.log("ENTRO AL SERVICIO");
-   }
+  constructor(private apollo: Apollo) {}
+
+  async registrarCurso(statement, answers, correct) {
+    
+    var ans1 = this.getQueryInsertAnswer(answers[0], correct == 1);
+    var ans2 = this.getQueryInsertAnswer(answers[1], correct == 2);
+    var ans3 = this.getQueryInsertAnswer(answers[2], correct == 3);
+    var ans4 = this.getQueryInsertAnswer(answers[3], correct == 4);
+
+    const result1 = await this.apollo.mutate<Response>({mutation: ans1}).toPromise()
+
+    console.log(result1.data)
+    console.log(result1.data.InsertAnswer)
+    console.log(result1.data.InsertAnswer.id)
+
+    const result2 = await this.apollo.mutate<Response>({mutation: ans2}).toPromise()
+
+    const result3 = await this.apollo.mutate<Response>({mutation: ans3}).toPromise()
+
+    const result4 = await this.apollo.mutate<Response>({mutation: ans4}).toPromise()
+
+    var a1 = result1.data.InsertAnswer.id;
+    var a2 = result2.data.InsertAnswer.id;
+    var a3 = result3.data.InsertAnswer.id;
+    var a4 = result4.data.InsertAnswer.id;
+
+    var question = this.getMutationInsertQuestion(statement, a1, a2, a3, a4);
+
+    const resultQuest = await this.apollo.mutate<ResponseQuestion>({mutation: question}).toPromise();
+
+    if(resultQuest.data.InsertQuestion.statement == statement) {
+      alert("Se registro correctamente");
+    } else {
+      alert("Ocurrio un error, no se pudo registrar el quiz")
+    }
+  }
 
 
-  registrarCurso() {
-    const CURSO_QUERY = gql`
+  getQueryInsertAnswer(context, is_correct) {
+    var ANSWER_QUERY = gql`
     mutation {
-      crearCurso(curso: {
-        nombre: "PYTHON PRUEBA3"
-        categoria: "PYTHON"
-        duracion: 31
-        idProfesor: 1
+      InsertAnswer(answer: {
+        context: "${context}",
+        is_correct: ${is_correct}
       }) {
-        nombre,
-        categoria,
-        duracion,
-        idProfesor
+        id
+        context
       }
     }
     `;
-    this.apollo.mutate({
-      mutation: CURSO_QUERY,
-      variables:{}
-    }).subscribe((data) => {
-      console.log("-----------------");
-      console.log(data.data);
-    },(error) => {
-      console.log(error);
-    })
+    return ANSWER_QUERY;
+  }
+
+
+  getMutationInsertQuestion(statement, ans1, ans2, ans3, ans4) {
+    var QUESTION_MUTATION = gql`
+    mutation {
+      InsertQuestion(
+        statement: "${statement}",
+        score: 1,
+        user_id:1,
+        answers: [
+          "${ans1}",
+          "${ans2}",
+          "${ans3}",
+          "${ans4}"
+        ],
+        qualification: []
+      ) {
+        statement
+      }
+    }
+    `;
+    return QUESTION_MUTATION;
   }
 }
